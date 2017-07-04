5b099170ed6013f9dd958d8e70667c1112ab9372
Juan Victor Cortez <juanvictor.cortez@gmail.com>
2017-07-04 06:24:00 +0000

    updated https request

--- download_avatars.js
+++ download_avatars.js
@@ -2,8 +2,8 @@ var request = require('request');
 
 console.log('Welcome to the Github Avatar Downloader!');
 
-var GITHUB_USER = "jv-cortez";
-var GITHUB_TOKEN = "dad600fe97dfc40673e2a7ed103b3ae02fe7f1d6";
+var GITHUB_USER = process.env.GITHUB_USER;
+var GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 var request = require('request');
 var fs = require('fs');
 
@@ -17,65 +17,35 @@ function getRequestOptions(repoOwner, repoName) {
 }
 
 function getRepoContributors(repoOwner, repoName, callback) {
-  request(getRequestOptions(repoOwner, repoName), function (error, response, body) {
+    request(getRequestOptions(repoOwner, repoName), function (error, response, body) {
     try {
       const data = JSON.parse(body);
       data.forEach((contributor) => {
-    console.log(contributor.avatar_url);
-      })
-      console.log(newData);    
-    } catch (err) {
-      console.log('Failed to parse content body');
-    }
-  });
+      console.log(contributor.avatar_url) ;
+        })  
+        } catch (err) {
+        console.log('Failed to parse content body');
+        }
+    });
 }
 
-
-// function downloadImageByUrl(url, filePath){
-//     request.get('url')
-
-//       .on('error', function (err) {
-//          throw err;
-//        })
-
-//       .on('response', function (response) {
-//          console.log('Response Status Code:\n',response.statusCode, response.headers['content-type']);
-//        })
-
-//       .pipe(fs.createWriteStream(filePath));
-//       console.log("Download complete...")
-// }
-// function getRepoContributors(repoOwner, repoName, callback){
-
+// function downloadImageByUrl(url, filePath) {
     
-//     var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-//     // console.log(requestURL)
+// request.get(url)
 
-//         request.get(requestURL) 
-//             .on('error', function (err) {
-//                 throw err;
-//             })
+//         .on('error', function (err) {
+//             throw err;
+//         })
 
-//             .on('reponse', function (response) {
-//                 console.log('Response Status Code:\n',response.statusCode, response.headers['content-type']);
-//             })
+//         .on('response', function (response) {
+//             console.log('Response Status Code:\n',response.statusCode,response.statusMessage, response.headers['content-type']);
+//         })
 
-//             .pipe(fs.createWriteStream('./body'));
-//             console.log("Download complete...")
-    
-//     try {
-//         const data = JSON.parse(body);
-//         callback(data);
-//     } catch (err) {
-//     console.log('Failed to parse content body', err);
+//         .pipe(fs.createWriteStream(filePath));
+//         console.log("Download complete...")
 //     }
-//     return { 
-//         headers : {
-//             'User-Agent': "LHL student project"
-//         }
-//     };
-// }
 
+// downloadImageByUrl("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "./kvirani.jpg")
 
 getRepoContributors("jquery", "jquery", function(err, result) {
     console.log("Errors:", err);
