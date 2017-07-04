2a193f3142544eada0cbf23cb52dc2825a2a2248
Juan Victor Cortez <juanvictor.cortez@gmail.com>
2017-07-04 01:36:04 +0000

    added request URL

--- download_avatars.js
+++ download_avatars.js
@@ -2,12 +2,19 @@ var request = require('request');
 
 console.log('Welcome to the Github Avatar Downloader!');
 
+var GITHUB_USER = "jv-cortez";
+var GITHUB_TOKEN = "dad600fe97dfc40673e2a7ed103b3ae02fe7f1d6";
+
 function getRepoContributors(repoOwner, repoName, callback){
-    
-}    
+            
+var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+    console.log(requestURL)
+}
 
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-
+    console.log("Errors:", err);
+    console.log("Result:", result);
 });
+
+
+
