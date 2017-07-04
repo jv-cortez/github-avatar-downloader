c82da7b1f62e1f4a3d532cacfd03efdab8e0cd42
Juan Victor Cortez <juanvictor.cortez@gmail.com>
2017-07-04 01:18:14 +0000

    calling function test

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,13 @@
 var request = require('request');
 
 console.log('Welcome to the Github Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, callback){
+    
+}    
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+
+});
